03d3f21ff7ea36f926e4221bc93f7aedaf9770d6
Robert Koteles <koteles.roberto@gmail.com>
2017-12-02 18:41:51 +0000

    Main navigation in header is done. Basic javascript structure has been created.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,64 @@
 *   2017
 *	BBCWeather application task
 ******************************************/
+
+/*********************
+*  GENERAL FUNCTIONS
+*********************/
+
+var GeneralFunctions = {
+    prevent_default: function () {
+        'use strict';
+        if (window.event) {
+            window.event.returnValue = false;
+        } else if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    }
+};
+
+/*********************
+*  SPECIFIC FUNCTIONS
+*********************/
+
+var Header = {
+    initialize: function () {
+        var self = this;
+    }
+};
+
+var Navigation = {
+    initialize: function () {
+        var self = this;
+    }
+};
+
+
+var Footer = {
+    initialize: function () {
+        var self = this;
+    }
+};
+
+
+
+var Services = {
+
+    initialize: function () {
+        var self = this;
+    }
+
+};
+
+
+$(function () {
+
+    Header.initialize();
+    Navigation.initialize();
+    Footer.initialize();
+
+    Services.initialize();
+
+});
